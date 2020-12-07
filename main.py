5792b148612d868cdfee6947768f7ec1d96c7e3e
Nicolò Valenza <nico.vale.spazio@outlook.com>
2020-12-07 20:06:58 +0100

    Update main.py
    add new api origin
    https://github.com/pcm-dpc/COVID-19

--- main.py
+++ main.py
@@ -1,20 +1,51 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from covid.api import CovId19Data
+import requests
+'''
+{
+  "confirmed": 1728878,
+  "deaths": 60078,
+  "label": "Italy",
+  "last_updated": "2020-12-06 00:00:00",
+  "lat": "41.87194",
+  "long": "12.56738",
+  "recovered": 913494
+}
+'''
+
+allow_cors = [
+    "https://www.liceocremonablog.it",
+    "http://www.liceocremonablog.it"
+]
 app = Flask(__name__)
-api = CovId19Data(force=True)
-res = api.filter_by_country("italy")
-cors = CORS(app, resources={r"/api": {"origins": "*"}})
-api = CovId19Data(force=False)
+cors = CORS(app, resources={r"/api": {"origins": allow_cors},r"/v2/api": {"origins": allow_cors},r"/v2/api/complete": {"origins": allow_cors},r"/v1/api": {"origins": allow_cors} })
 @app.route('/')
 def index():
-    return '<h1>index<h1><br><p>link to <a href="/api">api</a></p>'
+    return '<h1>index<h1><br><p>link to <a href="/v2/api">api</a></p>'
 
+@app.route('/v1/api')
+def api_old():
+    return jsonify({"body": "questa versione non è più disponibile"})
+
+@app.route('/v2/api')
 @app.route('/api')
 def api():
-    return res
-
-
+    raw_json = requests.get("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-andamento-nazionale.json").json()
+    today_data = raw_json[(len(raw_json)-1)]
+    api_dict = {
+        "confirmed": today_data["totale_casi"],
+        "deaths": today_data["deceduti"],
+        "recovered": today_data["dimessi_guariti"],
+        "last_updated": today_data["data"],
+        "label": today_data["stato"]
+    }
+    return jsonify(api_dict)
+    
+@app.route('/v2/api/complete')
+def api_or():
+    raw_json = requests.get("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-andamento-nazionale.json").json()
+    today_data = raw_json[(len(raw_json)-1)]
+    return jsonify(today_data)
 
 if __name__ == "__main__":
     app.run(debug=True)
